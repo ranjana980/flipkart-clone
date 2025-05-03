eea73d203401a7538ce5597a10719a0994d2a9bf
ranjana2408 <ranjana20@navgurukul.org>
2025-05-03 11:19:02 +0530

    be update

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 // Load .env file
-
+require('dotenv').config();
 const app = express();
 const productRoute = require('./routes/e-commerce')
 const bodyParser = require("body-parser")
@@ -10,7 +10,7 @@ app.use(express.json({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 mongoose.set('strictQuery', false);
-const uri = "mongodb+srv://ranjana20:sX2WLuV9vELvivta@e-commerce.439zbzn.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce";
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@e-commerce.439zbzn.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce`;
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
--- Frontend/src/api/baseUrl.js
+++ Frontend/src/api/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://e-commerce-backend-chi-dusky.vercel.app"
+export const baseUrl = "https://e-commerce-k5c9.vercel.app/"
 // export const baseUrl = "http://localhost:4000"
