43a5b7bdc410a618983b9821eb6d050e2877c8df
ranjana2408 <ranjana20@navgurukul.org>
2025-06-10 11:36:40 +0530

    base url changes

--- Frontend/src/api/baseUrl.js
+++ Frontend/src/api/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://e-commerce-backend-alpha-weld.vercel.app"
-export const baseUrl = "http://localhost:4000"
+export const baseUrl = "https://e-commerce-backend-alpha-weld.vercel.app"
+// export const baseUrl = "http://localhost:4000"
